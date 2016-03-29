2bdc7293bf86ea05feb936c58747ef10f1b76efe
eao197 <eao197@gmail.com>
2016-03-29 10:07:11 +0300

    Simple mxxru-wrapper added.

---
+++ dev/libmosquitto/prj.rb
@@ -0,0 +1,18 @@
+gem 'Mxx_ru', '>= 1.6.8'
+
+require 'mxx_ru/cpp'
+
+MxxRu::Cpp::dll_target {
+
+	target 'mosquitto-1.4.8'
+	implib_path 'lib'
+
+	include_path 'libmosquitto', Mxx_ru::Cpp::Target::OPT_UPSPREAD
+	include_path 'libmosquitto/lib', Mxx_ru::Cpp::Target::OPT_UPSPREAD
+
+	define 'WITH_THREADING'
+	define 'libmosquitto_EXPORTS'
+
+	c_sources Dir[ 'libmosquitto/lib/*.c' ]
+}
+
